c8485183b1d4e42c87004002c7b5475dd354520e
willy <willy.kuckelsberg@gmail.com>
2022-07-01 22:02:00 +0200

    [Fix 24] Fehler behoben

--- lib/flyController.h
+++ lib/flyController.h
@@ -13,19 +13,9 @@
 #include "axisYaw.h"
 #include "radio.h"
 #include "sonic.h"
+#include "model.h"
 
-typedef enum
-{
-    arming = 0, ///< When the Kuckycopter is first turned on, the arming starts.
-    fly_arming_busy,    /// ist das nötig?
-    fly_disablePID,
-    fly_standby,  ///< All motors on POWER_MIN
-    fly_prestart, ///< All motors on standby and ready to fly. (POWER_MIN)
-    fly_takeoff,  ///< The Quadrocopter takes off.
-    fly_set_pid,  ///< Fly without PID-Output = 0
-    fly_fly,      ///< Normal fly mode
-    fly_ground    ///< Kuckycopter stand on the ground
-} flyState_e;
+
 
 #define PIN_LED_STATE 7
 #define POWER_LIFT_UP 60 ///< The KuckyCopter will start, if throttle > 60
@@ -33,15 +23,16 @@ typedef enum
 #define PID_ACTIVE_AT 9     ///< PID aktiviert ab einer Höhe von 9cm
 class FlyController : public Task::Base
 {
-
+public:
+    
 private:
-    flyState_e _flyState;
-    AxisYaw *_axisYaw;
-    yawData_t *_yawData;
-    Radio *_radio;
-    Sonic *_sonic;
-    sonicData_t *_sonicData;
-
+    //flyState_e _flyState;
+     AxisYaw *_axisYaw;
+    // yawData_t *_yawData;
+    // Radio *_radio;
+    // Sonic *_sonic;
+    // sonicData_t *_sonicData;
+    model_t *_model;
 
 public:
     FlyController(const String &name)
@@ -51,132 +42,139 @@ public:
 
     virtual ~FlyController() {}
 
-    // FlyController *setModel(flyState_e *_model)
-    // { // Rückgabe wert ist das eigene Objekt (this)
-    //   //_axis_data  wird aus dem Model i9n die Achsed geschriene
-    //     LOGGER_VERBOSE("Enter....");
-    //     _flyState = _model;
-
-    //     LOGGER_VERBOSE("....leave");
-    //     return this;
-    // }
-
-    virtual void begin() override
-    {
+    FlyController *init(model_t *model){ // Rückgabe wert ist das eigene Objekt (this)
+      //_axis_data  wird aus dem Model i9n die Achsed geschriene
         LOGGER_VERBOSE("Enter....");
-        _flyState = arming;
+        _model = model;
+        _model->flyState = arming_begin;
+
         LOGGER_VERBOSE("....leave");
+        return this;
+    }
+
+    FlyController *setYawAxis(AxisYaw *axisYaw){
+        _axisYaw = axisYaw;
+        return this;
+    }
+
+    virtual void begin() override   // Wird nach dem Erzeugen das Tasks automatisch aufgerufen. _model ist hier noch nicht bekannt
+    {
+
     }
 
     virtual void update() override
     {
         static uint16_t downTime = 0;
-        switch (_flyState)
+        switch (_model->flyState)
         {
 
-        case arming:
+        case arming_begin:
             /* This is only setting, for the first start from the airplane.
-             * Main Power ON/ OFF or option switch. */
+             * Main Power ON/OFF or option-switch. */
             LOGGER_NOTICE("arming start");
-//            LOGGER_NOTICE("not implemented yet");
-            _axisYaw->setState(AxisYaw::arming_start);
-            _flyState = fly_arming_busy;
+            _axisYaw->setState(AxisYaw::arming_start);     /// hier bleibt es hängen
+            _model->flyState = arming_busy;
+            LOGGER_NOTICE("Leave arming start");
             break;
 
         //case AxisMotor::arming_busy:  // geändert 27-06-2022
-        case fly_arming_busy:
+        case arming_busy:
             LOGGER_NOTICE("arming is finished");
-//            LOGGER_NOTICE("not implemented yet");
             if (_axisYaw->isArmed())
             {
-                _flyState = fly_disablePID; /// -> goes to "disablePID"
+                _model->flyState = disablePID; 
             }
             break;
 
-        case fly_disablePID:
-            /* Deactivate the PID controller from all axis, and report the state. */
+        case disablePID:
+            /* Deactivate the PID controller from all axis. */
             LOGGER_NOTICE("disablePID");
-//            LOGGER_NOTICE("not implemented yet");
-            _axisYaw->setState(AxisYaw::disablePID);
-            _flyState = fly_standby;
+            _axisYaw->setState(AxisYaw::state_e::disablePID);
+            _model->flyState = standby;
             LOGGER_NOTICE("disable PID is finished");
             break;
 
-        case fly_standby:
+        case standby:
             /* Make sure the throttle lever is set to 0 and RC is connected. */
-            //_radio->interface->isconnect = true;
-            //_radio->interface->payload.rcThrottle = 1;
+            //_radio->interface->isconnect = true;          // nur zum testen, ob Flycontroller
+            //_radio->interface->payload.rcThrottle = 1;    // durchläuft
             LOGGER_NOTICE("standby");
 //            LOGGER_NOTICE("not implemented yet");
-            if (_radio->interface->isconnect && (_radio->interface->payload.rcThrottle <= POWER_MIN))
+            if (_model->interface.isconnect && (_model->interface.payload.rcThrottle <= POWER_MIN))
             {
-                _flyState = fly_prestart;
+                _model->flyState = prestart;
             }
             else
             {
-                _yawData->throttle = 0;
-                _flyState = fly_standby;
+                _model->yawData.throttle = 0;
+                _model->flyState = standby;
             }
             LOGGER_NOTICE("standby fineshed");
             break;
 
-        case fly_prestart:
+        case prestart:
             LOGGER_NOTICE("prestart");
 //            LOGGER_NOTICE("not implemented yet");
             /* Checked if all axes are OK. */
             _axisYaw->setState(AxisYaw::ready);
             if (_axisYaw->isReady())
             {
-                _flyState = fly_takeoff;
+                _model->flyState = takeoff;
             }
             else
             {
-                _flyState = fly_prestart; /// new 23.05.21
+                _model->flyState = prestart; 
             }
             break;
 
-            case fly_takeoff:
+            case takeoff:
                 LOGGER_NOTICE("take off");
                 LOGGER_NOTICE("not implemented yet");
                 /* Throttle greater than POWER_LIFT_UP and RC is connected, go to the next state. */
-                 _yawData->throttle = _radio->interface->payload.rcThrottle;
-                if (_radio->interface->isconnect && (_radio->interface->payload.rcThrottle < POWER_LIFT_UP))
+                //_radio->interface->isconnect = true;          // nur zum testen, ob Flycontroller           
+                 _model->yawData.throttle = _model->interface.payload.rcThrottle;
+                //_radio->interface->payload.rcThrottle = 1;    // durchläuft
+                if (_model->interface.isconnect && (_model->interface.payload.rcThrottle < POWER_LIFT_UP))
                 {
-                    _flyState = fly_set_pid;
+                    _model->flyState = set_pid;
                 }
                 else
                 {
-                    _flyState = fly_takeoff;
+                    _model->flyState = takeoff;
                 }
                 LOGGER_NOTICE("take off fineshed");
                 break;
 
-        case fly_set_pid:
+        case set_pid:
             /* If everything is checked, the PID controller is activated. */
             LOGGER_NOTICE("FlyController set_pid");
 //            LOGGER_NOTICE("not implemented yet");
-            _yawData->throttle = _radio->interface->payload.rcThrottle;
-            if (_radio->interface->isconnect && (_radio->interface->payload.rcThrottle >= POWER_LIFT_UP))
+            //_radio->interface->isconnect = true;          // nur zum testen, ob Flycontroller           
+            _model->yawData.throttle = _model->interface.payload.rcThrottle;
+            //_radio->interface->payload.rcThrottle = 1;    // durchläuft
+            if (_model->interface.isconnect && (_model->interface.payload.rcThrottle >= POWER_LIFT_UP))
             {
                 _axisYaw->setState(AxisYaw::enablePID);
-                _yawData->horz_Position = 0; ///< Reset YAW Position before lift off
-                _flyState = fly_fly;
+                _model->yawData.horz_Position = 0; ///< Reset YAW Position before lift off
+                _model->flyState = fly;
             }
             else
             {
-                _flyState = fly_set_pid; /// new 23.05.21
+                _model->flyState = set_pid; /// new 23.05.21
             }
             break;
 
-        case fly_fly:
+        case fly:
             /* If the power is less than POWER_LIFT_UP and the altitude is less than PID_ACTIVE_AT, the status is set to ground. */
             LOGGER_NOTICE("fly");
 //            LOGGER_NOTICE("not implemented yet");
-            _yawData->throttle= _radio->interface->payload.rcThrottle;
-            if ((_radio->interface->payload.rcThrottle <= POWER_LIFT_UP) || (_sonicData->distance < PID_ACTIVE_AT))
+                //_sonicData->distance > 9;          // nur zum testen, ob Flycontroller             
+            _model->yawData.throttle= _model->interface.payload.rcThrottle;
+                //_radio->interface->payload.rcThrottle >= 60;    // durchläuft
+            if ((_model->interface.payload.rcThrottle <= POWER_LIFT_UP) || (_model->sonicData.distance < PID_ACTIVE_AT))
             //if ((_radio->interface->payload.rcThrottle <= POWER_LIFT_UP))
             {
-                _flyState = fly_ground;
+                _model->flyState = ground;
             }
             else
             {
@@ -185,24 +183,24 @@ public:
             }
             break;
 
-        case fly_ground:
+        case ground:
             /* If the quadrocopter is on the ground for more than DOWN_TIME, disable the engines. */
             LOGGER_NOTICE("ground");
 //            LOGGER_NOTICE("not implemented yet");
 
             if (millis() - downTime >= DOWN_TIME)
             {
-                _flyState = fly_fly;
+                _model->flyState = fly;
             }
             else
             {
-                _flyState = fly_disablePID;
-                _yawData->throttle = 0;
+                _model->flyState = disablePID;
+                _model->yawData.throttle = 0;
             }
             break;
 
         default:
-            _flyState = fly_disablePID;
+            _model->flyState = disablePID;
             break;
 
         } /* end of switch flyState */
