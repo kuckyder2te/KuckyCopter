5ae8b5b1d2f49fc6979a5c43f5241c3e8fca09f5
Willy <willy@kuckelsberg.de>
2022-10-09 20:18:41 +0200

    [Change 119a]

--- lib/sonic.h
+++ lib/sonic.h
@@ -3,12 +3,12 @@
  *	Project name: KuCo_Phantom 1
  *  Date: 2022-05-28
  *  Author: Wilhelm Kuckelsberg
- *  Description: Niederflug 
+ *  Description: Niederflug
  *  HCSR04 max. Distanz 400cm
- *  
+ *
  *  Impulsdauer 100cm   2,9137 mSec * 2 = 5,8275 mSec
- *              200cm   5,8275            11,655 mSec        
- */             
+ *              200cm   5,8275            11,655 mSec
+ */
 
 #include <Arduino.h>
 #include <TaskManager.h>
@@ -21,7 +21,7 @@
 #define PIN_ECHO 21
 #define PIN_TRIGGER 22
 #define PIN_DHT 28
-#define DHTTYPE    DHT22
+#define DHTTYPE DHT22
 
 #define MAX_DISTANCE 200
 #define MAX_TIME_OUT 11655
@@ -29,6 +29,7 @@
 typedef struct
 {
     float temperature;
+    float humidity;
     double distance;
 } sonicData_t;
 class Sonic : public Task::Base
@@ -66,32 +67,28 @@ public:
         _dht = new DHT_Unified(PIN_DHT, DHTTYPE);
 
         _dht->begin();
-            Serial2.println(F("DHTxx Unified Sensor Example"));
-            // Print temperature sensor details.
-            sensor_t sensor;
-            _dht->temperature().getSensor(&sensor);
-            Serial2.println(F("------------------------------------"));
-            Serial2.println(F("Temperature Sensor"));
-            Serial2.print  (F("Sensor Type: ")); Serial2.println(sensor.name);
-            Serial2.print  (F("Driver Ver:  ")); Serial2.println(sensor.version);
-            Serial2.print  (F("Unique ID:   ")); Serial2.println(sensor.sensor_id);
-            Serial2.print  (F("Max Value:   ")); Serial2.print(sensor.max_value); Serial2.println(F("°C"));
-            Serial2.print  (F("Min Value:   ")); Serial2.print(sensor.min_value); Serial2.println(F("°C"));
-            Serial2.print  (F("Resolution:  ")); Serial2.print(sensor.resolution); Serial2.println(F("°C"));
-            Serial2.println(F("------------------------------------"));
-            // Print humidity sensor details.
-            _dht->humidity().getSensor(&sensor);
-            Serial2.println(F("Humidity Sensor"));
-            Serial2.print  (F("Sensor Type: ")); Serial2.println(sensor.name);
-            Serial2.print  (F("Driver Ver:  ")); Serial2.println(sensor.version);
-            Serial2.print  (F("Unique ID:   ")); Serial2.println(sensor.sensor_id);
-            Serial2.print  (F("Max Value:   ")); Serial2.print(sensor.max_value); Serial2.println(F("%"));
-            Serial2.print  (F("Min Value:   ")); Serial2.print(sensor.min_value); Serial2.println(F("%"));
-            Serial2.print  (F("Resolution:  ")); Serial2.print(sensor.resolution); Serial2.println(F("%"));
-            Serial2.println(F("------------------------------------"));
-            
-            // Set delay between sensor readings based on sensor details.
-            delayMS = sensor.min_delay / 1000;
+        sensor_t sensor;
+        _dht->temperature().getSensor(&sensor);
+        LOGGER_WARNING("Temperature Sensor");
+        LOGGER_WARNING_FMT("Sensor Type: %c", sensor.name);
+        LOGGER_WARNING_FMT("Driver Ver: %i", sensor.version);
+        LOGGER_WARNING_FMT("Unique ID: %i", sensor.version);
+        LOGGER_WARNING_FMT("Min  %f max. value %f ", sensor.min_value, sensor.max_value);
+        LOGGER_WARNING_FMT("Resolution:  %f", sensor.resolution);
+
+        // Print humidity sensor details.
+        _dht->humidity().getSensor(&sensor);
+        LOGGER_WARNING("Humidity Sensor");
+        LOGGER_WARNING_FMT("Sensor Type: %c", sensor.name);
+        LOGGER_WARNING_FMT("Driver Ver: %i", sensor.version);
+        LOGGER_WARNING_FMT("Unique ID: %i", sensor.version);
+        LOGGER_WARNING_FMT("Min  %f max. value %f ", sensor.min_value, sensor.max_value);
+        LOGGER_WARNING_FMT("Resolution:  %f", sensor.resolution);
+
+        // Set delay between sensor readings based on sensor details.
+        delayMS = sensor.min_delay / 1000;  // ~2000
+        Serial2.println(delayMS);
+//        delay(5000);
         LOGGER_VERBOSE("....leave");
     } /*--------------------- end og begin --------------------------------------------*/
 
@@ -105,38 +102,37 @@ public:
     virtual void update() override
     {
         LOGGER_VERBOSE("Enter....");
-        //   LOGGER_NOTICE_FMT("Temperature: %.2f *C", _sonicData->temperature);
+       
         LOGGER_WARNING_FMT("Distance: %.2f cm", _hcrs04->measureDistanceCm());
-        
-         // Delay between measurements.
-        delay(delayMS);
+
+        // Delay between measurements.
+    //    delay(delayMS);
         // Get temperature event and print its value.
         sensors_event_t event;
         _dht->temperature().getEvent(&event);
-        if (isnan(event.temperature)) {
-            Serial2.println(F("Error reading temperature!"));
+        if (isnan(event.temperature))
+        {
+            LOGGER_FATAL("Error reading temperature!");
         }
-        else {
-            Serial2.print(F("Temperature: "));
-            Serial2.print(event.temperature);
-            Serial2.println(F("°C"));
+        else
+        {
+            _sonicData->temperature = event.temperature;
+            LOGGER_WARNING_FMT("Temperature: %.2f%s", event.temperature, "*C");
         }
         // Get humidity event and print its value.
         _dht->humidity().getEvent(&event);
-        if (isnan(event.relative_humidity)) {
-            Serial2.println(F("Error reading humidity!"));
+        if (isnan(event.relative_humidity))
+        {
+            LOGGER_FATAL("Error reading humidity!");
         }
-        else {
-            Serial2.print(F("Humidity: "));
-            Serial2.print(event.relative_humidity);
-            Serial2.println(F("%"));
+        else
+        {
+            _sonicData->humidity = event.relative_humidity;
+            LOGGER_WARNING_FMT("Humidity: %.2f%c", event.relative_humidity, '%');
         }
-        
-        
-        
         LOGGER_VERBOSE("....leave");
     } /*--------------------- end og update -------------------------------------------*/
 
-};/*----------------------------------- end of sonic.h class --------------------------*/
+}; /*----------------------------------- end of sonic.h class --------------------------*/
 
 //#undef _DEBUG_
