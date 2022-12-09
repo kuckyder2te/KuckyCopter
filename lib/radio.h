48c10268c43664901039cdc558e814e1aa010e09
Willy <willy.kuckelsberg@gmail.com>
2022-12-09 20:43:11 +0100

    [Fix 001]

--- lib/radio.h
+++ lib/radio.h
@@ -26,7 +26,7 @@
 #include <printf.h>   //funktioniert hier nicht
 #include <RF24.h>
 
-//#define LOCAL_DEBUG
+#define LOCAL_DEBUG
 #include "myLogger.h"
 
 #define PIN_RADIO_CE 20
@@ -62,16 +62,21 @@ typedef struct
     RX_payload_t RX_payload; 
 } RC_interface_t;
 
+
+
+
 class Radio : public Task::Base
 {
     bool radioNumber; // 0 uses pipe[0] to transmit, 1 uses pipe[1] to received
-    bool role;        // true(>0) = TX role, false(0) = RX role
+    bool role, _role,__role,___role;        // true(>0) = TX role, false(0) = RX role
     const uint64_t pipe[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
-
+    RX_payload_t _RX_payload;
+    unsigned long _lastReceivedPacket;
 protected:
     RF24 *_radio; 
     RC_interface_t *RC_interface;   
-    TX_payload_t TX_payload;
+    TX_payload_t TX_payload, _TX_payload;
+
 
 public:
     Radio(const String &name)
@@ -102,7 +107,10 @@ public:
             {} 
         }
 
-        role = true;        // Parameter als Empfänger
+        role = false;        // Parameter als Empfänger
+        _role = !role;
+        __role = _role;
+        ___role = _role;
         radioNumber = 1; 
         
         _radio->setPALevel(RF24_PA_LOW);  // RF24_PA_MAX is default.
@@ -110,13 +118,13 @@ public:
         _radio->enableAckPayload();
         _radio->openWritingPipe(pipe[radioNumber]);     // always uses pipe 0
         _radio->openReadingPipe(1, pipe[!radioNumber]); // using pipe 1
-        _radio->stopListening(); 
+        _radio->startListening(); 
 
         TX_payload.yaw = 129.9;
         TX_payload.pitch = -12.8;
         TX_payload.roll = 41.2;
         TX_payload.altitude = 111;
-        TX_payload.sonic = 222;
+        //TX_payload.sonic = 222;
         TX_payload.temperature = 24.5;
         TX_payload.pressure = 12345.0;
   
@@ -128,42 +136,58 @@ public:
     virtual void update() override
     {
     LOGGER_VERBOSE("Enter....");  
-    
-        if (role) {// This device is a TX node
+
+        if (!role) {// This device is a TX node
             unsigned long start_timer = micros(); 
-            LOGGER_NOTICE_FMT("sizeod RX_payload = %i" ,sizeof(TX_payload_t));                 
-            bool report = _radio->write(&TX_payload, sizeof(TX_payload_t)); 
-            LOGGER_NOTICE_FMT("Report write = %i Number = %i Role = %i",report, radioNumber, role);
+
+            LOGGER_NOTICE_FMT_CHK(role, _role,"sizeof TX_payload = %i" ,sizeof(TX_payload_t));  
+            bool report = true;//_radio->write(&TX_payload, sizeof(TX_payload_t)); 
+            LOGGER_NOTICE_FMT_CHK(role, __role,"Report write = %i Number = %i Role = %i",report, radioNumber, role);
             unsigned long end_timer = micros();                    
             if (report) {
-                LOGGER_NOTICE_FMT("Transmission successful! time to transmit = %u",(end_timer - start_timer)); 
-               
-                LOGGER_NOTICE_FMT("Sent Yaw = %f", (float)TX_payload.yaw);
-                LOGGER_NOTICE_FMT("Sent Pitch = %f" ,(float)TX_payload.pitch);
-                LOGGER_NOTICE_FMT("Sent Roll = %f" ,(float)TX_payload.roll);
-                LOGGER_NOTICE_FMT("Sent Altitude = %f" ,(float)TX_payload.altitude);
-                LOGGER_NOTICE_FMT("Sent Sonic = %i" ,(int)TX_payload.sonic);
-                LOGGER_NOTICE_FMT("Sent Temperatur =%f" ,(float)TX_payload.temperature);
-                LOGGER_NOTICE_FMT("Sent Pressure = %f" ,(float)TX_payload.pressure);
+                //LOGGER_NOTICE_FMT("Transmission successful! time to transmit = %u",(end_timer - start_timer)); 
+                if(!_radio->writeAckPayload(1,&TX_payload,sizeof(TX_payload_t))){
+                    //LOGGER_NOTICE("ACK Pipe full");
+                }else{
+                    TX_payload.sonic++;
+                    //LOGGER_NOTICE("ACK Pipe empty");
+                }
+                LOGGER_NOTICE_FMT_CHK(TX_payload.yaw,_TX_payload.yaw,"Sent Yaw = %f", (float)TX_payload.yaw);
+                LOGGER_NOTICE_FMT_CHK(TX_payload.pitch,_TX_payload.pitch,"Sent Pitch = %f" ,(float)TX_payload.pitch);
+                LOGGER_NOTICE_FMT_CHK(TX_payload.roll,_TX_payload.roll,"Sent Roll = %f" ,(float)TX_payload.roll);
+                LOGGER_NOTICE_FMT_CHK(TX_payload.altitude,_TX_payload.altitude,"Sent Altitude = %f" ,(float)TX_payload.altitude);
+                LOGGER_NOTICE_FMT_CHK(TX_payload.sonic,_TX_payload.sonic,"Sent Sonic = %i" ,(int)TX_payload.sonic);
+                LOGGER_NOTICE_FMT_CHK(TX_payload.temperature,_TX_payload.temperature,"Sent Temperatur =%f" ,(float)TX_payload.temperature);
+                LOGGER_NOTICE_FMT_CHK(TX_payload.pressure,_TX_payload.pressure,"Sent Pressure = %f" ,(float)TX_payload.pressure);
 
                 uint8_t pipe;
                 if (_radio->available(&pipe)) {  // is there an ACK payload? grab the pipe number that received it
+                    _lastReceivedPacket = millis();
                     _radio->read(&RC_interface->RX_payload, sizeof(RC_interface_t));  // get incoming ACK payload   
                     // hier muss 1 - 1 - 1 kommen
-                    LOGGER_NOTICE_FMT("Report read = %i Number = %i Role = %i",report, radioNumber, role);
-                    LOGGER_NOTICE_FMT("Recieved %i bytes on %i ",_radio->getDynamicPayloadSize(), pipe);
-                    LOGGER_NOTICE_FMT("Read Throttle = %i",(int)RC_interface->RX_payload.rcThrottle);    
-                    LOGGER_NOTICE_FMT("Read YAW = %f",(float)RC_interface->RX_payload.rcYaw);
-                    LOGGER_NOTICE_FMT("Read Pitch = %f",(float)RC_interface->RX_payload.rcPitch);
-                    LOGGER_NOTICE_FMT("Read Roll = %f",(float)RC_interface->RX_payload.rcRoll);
-                    LOGGER_NOTICE_FMT("Read SW1 = %i",(int)RC_interface->RX_payload.rcSwi1);
-                    LOGGER_NOTICE_FMT("Read SW2 = %i",(int)RC_interface->RX_payload.rcSwi2);
-                    LOGGER_NOTICE_FMT("Read SW3 = %i",(int)RC_interface->RX_payload.rcSwi3);
-                    LOGGER_NOTICE_FMT("Read Check = %f",(float)RC_interface->RX_payload.checksum);
+                    LOGGER_NOTICE_FMT_CHK(role,___role,"Report read = %i Number = %i Role = %i",report, radioNumber, role);
+                    //LOGGER_NOTICE_FMT_CHK("Recieved %i bytes on %i ",_radio->getDynamicPayloadSize(), pipe);
+                    LOGGER_NOTICE_FMT_CHK(RC_interface->RX_payload.rcThrottle,_RX_payload.rcThrottle,"Read Throttle = %i",(int)RC_interface->RX_payload.rcThrottle);    
+                    LOGGER_NOTICE_FMT_CHK(RC_interface->RX_payload.rcYaw,_RX_payload.rcYaw,"Read YAW = %f",(float)RC_interface->RX_payload.rcYaw);
+                    LOGGER_NOTICE_FMT_CHK(RC_interface->RX_payload.rcPitch,_RX_payload.rcPitch,"Read Pitch = %f",(float)RC_interface->RX_payload.rcPitch);
+                    LOGGER_NOTICE_FMT_CHK(RC_interface->RX_payload.rcRoll,_RX_payload.rcRoll,"Read Roll = %f",(float)RC_interface->RX_payload.rcRoll);
+                    LOGGER_NOTICE_FMT_CHK(RC_interface->RX_payload.rcSwi1,_RX_payload.rcSwi1,"Read SW1 = %i",(int)RC_interface->RX_payload.rcSwi1);
+                    LOGGER_NOTICE_FMT_CHK(RC_interface->RX_payload.rcSwi2,_RX_payload.rcSwi2,"Read SW2 = %i",(int)RC_interface->RX_payload.rcSwi2);
+                    LOGGER_NOTICE_FMT_CHK(RC_interface->RX_payload.rcSwi3,_RX_payload.rcSwi3,"Read SW3 = %i",(int)RC_interface->RX_payload.rcSwi3);
+                    LOGGER_NOTICE_FMT_CHK(RC_interface->RX_payload.checksum,_RX_payload.checksum,"Read Check = %f",(float)RC_interface->RX_payload.checksum);
                     
                     _radio->writeAckPayload(1, &TX_payload, sizeof(TX_payload_t));
                 } else {
-                    LOGGER_FATAL(" Recieved: an empty ACK packet");  // empty ACK packet received
+                    if(millis()-_lastReceivedPacket>10000){
+                        static unsigned long _lastmillis = millis();
+                        if(millis()-_lastmillis>5000){
+                            LOGGER_NOTICE(" Recieved: an empty ACK packet");  // empty ACK packet received
+                            _lastmillis = millis();
+                        }
+                        RC_interface->isconnect = false;
+                    }else{
+                        RC_interface->isconnect = true;
+                    }
                 }
             } else {
                 LOGGER_FATAL("Transmission failed or timed out");  // payload was not delivered
