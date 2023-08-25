25b9ec02fc8c95a277096d2ce808db982d9966e8
Willy <willy.kuckelsberg@gmail.com>
2023-08-25 20:33:41 +0200

    Battery test included

--- test/sensor_radio_test.h
+++ test/sensor_radio_test.h
@@ -4,12 +4,14 @@
 #include "..\lib\sonics.h"
 #include "..\lib\monitor.h"
 #include "..\lib\model.h"
+#include "..\lib\battery.h"
 
 extern model_t model;
 
 Radio *radio;
 Sensor *sensor;
 Monitor *monitor;
+Battery *battery;
 
 void update_TX_payload()
 {
@@ -21,7 +23,7 @@ void update_TX_payload()
   model.RC_interface.TX_payload.distance_front = model.sonicData.front_distance;
   model.RC_interface.TX_payload.pressure = model.sensorData.pressure;
   model.RC_interface.TX_payload.temperature = model.sensorData.temperature_baro;
-  model.RC_interface.TX_payload.battery = 50;
+  model.RC_interface.TX_payload.battery = model.batteryData.battery_State;
 }
 
 void test_setup()
@@ -33,6 +35,9 @@ void test_setup()
   sensor = new Sensor("sensor");
   sensor->setModel(&model.sensorData)->begin();
 
+  battery = new Battery("battery");
+  battery->setModel(&model.batteryData)->begin();
+
   monitor = new Monitor("monitor", Report_t::RADIO_SENSOR);
   monitor->setModel(&model)->begin();
 }
@@ -40,6 +45,7 @@ void test_loop()
 {
   radio->update();
   sensor->enter();
+  battery->update();
   monitor->update();
   update_TX_payload();
   delay(10);
